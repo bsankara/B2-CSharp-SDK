70188471e27f203628e912f1a7c18fd0f7c1783c
balajisankar1 <balajisankar1@gmail.com>
2015-11-20 20:50:13 -0700

    Added JSON library and authorization

---
+++ B2-CSharp-SDK/B2-CSharp-SDK/B2SDK.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2_CSharp_SDK
+{
+    class B2SDK
+    {
+        static string BASE_BACKBLAZE_URL = "https://api.backblaze.com/b2api/v1/";
+
+        string accountID;
+        string apiUrl;
+        string authorizationToken;
+        string downloadURL;
+        bool authorized = false;
+
+        public B2SDK(string accountId, string applicationKey)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BASE_BACKBLAZE_URL + "b2_authorize_account");
+            string credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(accountId + ":" + applicationKey));
+            request.Headers.Add("Authorization", "Basic " + credentials);
+            request.ContentType = "application/json; charset=utf-8";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                response.Close();
+
+                dynamic jsonData = JsonConvert.DeserializeObject(responseString);
+
+                // set Data
+                accountID = accountId;
+                apiUrl = jsonData.apiUrl;
+                authorizationToken = jsonData.authorizationToken;
+                downloadURL = jsonData.downloadURL;
+                authorized = true;
+            }
+        }
+    }
+}
--- B2-CSharp-SDK/B2-CSharp-SDK/Program.cs
+++ B2-CSharp-SDK/B2-CSharp-SDK/Program.cs
@@ -10,6 +10,9 @@ namespace B2_CSharp_SDK
     {
         static void Main(string[] args)
         {
+            string accountId = Console.ReadLine();
+            string applicationKey = Console.ReadLine();
+            B2SDK sdk = new B2SDK(accountId, applicationKey);
         }
     }
 }
